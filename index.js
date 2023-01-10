f95fb1d38b13c14d7a8e71fc065f3bab9302cc44
Sidharth Baby <sidharthbaby77@gmail.com>
2023-01-10 21:07:34 +0530

    lasttt

--- index.js
+++ index.js
@@ -94,10 +94,14 @@ app.use(helmet.crossOriginResourcePolicy({ "policy": "cross-origin" }))
 app.use(morgan("common"))
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-    origin:["https://ednox.netlify.app","http://localhost:3000"],
-    credentials:true
-  }));
+// app.use(cors({
+//     origin:["https://ednox.netlify.app","http://localhost:3000"],
+//     credentials:true
+//   }));
+
+app.options('*', cors());
+app.use(cors());
+
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 //FILE STORAGE
